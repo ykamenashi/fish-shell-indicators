d39fe32bd186da840d30939dbd748a1a78b21e29
ykamenashi <kamenashi@cloud-ace.jp>
2019-12-24 13:18:05 +0900

    [prepare] setup file

---
+++ fish_prompt-part.fish
@@ -0,0 +1,15 @@
+#function fish_prompt 
+#
+  # ===
+  # append gadgets
+  # ===
+  echo ''
+  gcp_profile_indicator
+  echo -n ' '
+  terraform_ws_indicator
+  echo ''
+  # ===
+  # end
+  # ===
+#
+#end
